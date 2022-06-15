be188b14a8b9758a4551a03efe643b0af9dd6193
linga nikhil kumar <nikhilkumar435@gmail.com>
2022-06-15 09:36:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,14 @@
+import os
 tags=["2.9.5.1.4_props",'3.0.0.0.1.1.a','3.0.0.0.1.2.a']
 
-cluster="a"
+cluster=os.environ.get("cluster")
 current_cluster_tags = []
 for tag in tags:
     if cluster in tag:
         current_cluster_tags.append(tag.replace(".","").rstrip(cluster))
-
-
 ver = max(current_cluster_tags)
-
-print(ver)
 latest_version = int(ver) + 1
-
-print(latest_version)
-
 L_Version = '.'.join(str(latest_version)[i:i + 1] for i in range(0, len(str(latest_version)), 1))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+NEW_VERSION = f'{L_Version}.{cluster}'
+print(NEW_VERSION)
+os.environ['NEW_VERSION'] = NEW_VERSION
