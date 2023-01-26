245b605ecc08a33f4e21ef3f23c8537d99a7dab5
linga nikhil kumar <nikhilkumar435@gmail.com>
2023-01-26 22:57:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ for tag in tags:
     if cluster in tag:
         current_cluster_tags.append(tag.replace(".","").rstrip(cluster))
 ver = max(current_cluster_tags)
+os.environ['NAME'] = os.environ['NAME']+"test"
 latest_version = int(ver) + 1
 L_Version = '.'.join(str(latest_version)[i:i + 1] for i in range(0, len(str(latest_version)), 1))
 NEW_VERSION = f'{L_Version}.{cluster}'
